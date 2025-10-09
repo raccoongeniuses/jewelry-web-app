7e0ec6a2f4cc2cb6db1623766341eeb90667b404
hiromihanson <hiromihanson@crimson.network>
2025-10-09 13:21:44 +0700

    Update various HTML, CSS, and JS files across the project, including enhancements to the layout and styling of blog, product, and shop pages. Adjustments made to assets and documentation for improved functionality and user experience.

---
+++ corano/assets/js/draggable-grid/utils.js
@@ -0,0 +1,9 @@
+export const preloadImages = (selector = "img") => {
+  return new Promise((resolve) => {
+    imagesLoaded(
+      document.querySelectorAll(selector),
+      { background: true },
+      resolve
+    );
+  });
+};
