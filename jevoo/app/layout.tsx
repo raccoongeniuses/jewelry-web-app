783b2b97ba5e5d060f9a615ce5c779529782abf5
hiromihanson <hiromihanson@crimson.network>
2025-11-03 15:13:06 +0700

    fix: Merge font build

--- jevoo/app/layout.tsx
+++ jevoo/app/layout.tsx
@@ -1,18 +1,10 @@
 import type { Metadata } from "next";
-import { Lato } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import { CartProvider } from "../contexts/CartContext";
 import { AuthProvider } from "../contexts/AuthContext";
 import { WishlistProvider } from "../components/wishlist/WishlistProvider";
 import ModernizrInit from "../components/ModernizrInit";
-
-const lato = Lato({
-  weight: ['300', '400', '700', '900'],
-  style: ['normal', 'italic'],
-  subsets: ["latin"],
-  variable: "--font-lato",
-});
 
 export const metadata: Metadata = {
   title: "Jevoo Jewellery - Jewelry Shop eCommerce",
@@ -38,7 +30,7 @@ export default function RootLayout({
         <link rel="stylesheet" href="/assets/css/style.css" />
         <link rel="stylesheet" href="/assets/css/draggable-grid.css" />
       </head>
-      <body className={`${lato.variable} antialiased`} suppressHydrationWarning>
+      <body className="antialiased" suppressHydrationWarning>
         <AuthProvider>
           <CartProvider>
             <WishlistProvider>
